b6419b39e314712d3267ec8c2c81b7fda04a4c86
gnoul_ <ttluong4869@gmail.com>
2021-10-01 15:46:49 +0700

    Update product.controller.js

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,6 +1,8 @@
+const product = require('../models/product.model');
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('Testing new error');
-    res.status(200).json({ message: 'Products testing route' });
+    const result = await product.find({});
+    res.status(200).json({ result, total: result.length });
 };
 
 const getAllProducts = async (req, res) => {
