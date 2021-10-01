47e371c14f2fe9e4ac9870e966c992f54ad0c364
gnoul_ <ttluong4869@gmail.com>
2021-10-01 12:00:15 +0700

    Update product.model.js

--- models/product.model.js
+++ models/product.model.js
@@ -24,7 +24,7 @@ const productSchema = new mongoose.Schema({
     company: {
         type: String,
         enum: {
-            values: ['ikea', 'liddy', 'caressa', 'macros'],
+            values: ['ikea', 'liddy', 'caressa', 'marcos'],
             message: '{VALUE} is not supported',
         },
         // enum: ['ikea', 'liddy', 'caressa', 'macros']
