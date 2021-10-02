15965c5bc981088d2cfc0f70bcb6c0df61c27614
gnoul_ <ttluong4869@gmail.com>
2021-10-03 00:40:50 +0700

    Update product.controller.js

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,15 +1,35 @@
 const product = require('../models/product.model');
 
 const getAllProductsStatic = async (req, res) => {
-    const result = await product.find({
-        page: 2,
-    });
+    const result = await product.find({}).sort('price');
     res.status(200).json({ result, total: result.length });
 };
 
 const getAllProducts = async (req, res) => {
-    const result = await product.find(req.query);
-    res.status(200).json({ result, total: result.length });
+    const { name, isFeatured, company, sort } = req.query;
+    const queryObject = {};
+
+    if (isFeatured) {
+        queryObject.isFeatured = isFeatured;
+    }
+
+    if (company) {
+        queryObject.company = company;
+    }
+
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' };
+    }
+
+    console.log(queryObject);
+    let result = product.find(queryObject);
+    if (sort) {
+        const sortList = sort.split(',').join(' ');
+        result = await result.sort(sortList);
+    } else {
+        result = await result.sort('price');
+    }
+    res.status(200).json({ total: result.length, result });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
